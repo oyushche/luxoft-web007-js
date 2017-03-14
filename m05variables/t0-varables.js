37f895f9fe5a2a88deb0790b188e377fa946392b
oyushche <oyushche@gmail.com>
2017-03-14 09:44:25 +0200

    + vars task

---
+++ m05variables/t0-varables.js
@@ -0,0 +1,12 @@
+(function ()
+{
+
+    var catPrice = 20.00;
+    var dogPrice = 22.00;
+    var cowPrice = 70.00;
+
+    // 0. Print average check after selling of 20 cats, 16 dogs, and 3 cows
+
+    // 1. Add price of a penguin and recalculate average check after selling 10 more penguins
+
+})();
