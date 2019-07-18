df5fc5adb1c3ae1195f01ce155dcb01be4f6b64a
oyushche <uhmateush@gmail.com>
2019-07-18 02:49:19 -0400

    update scopes

--- m09functions/scopes.js
+++ m09functions/scopes.js
@@ -3,13 +3,21 @@ function F()
     // var self = this;
     this.a = 10;
     
-    this.test = () =>
+    this.test = function()
     {
-        
+        this.b = 400;
         console.log(this.a);
+        console.log(this.b);
     }
     
-    setTimeout(this.test, 0);
+    // this.test();
+    //
+    // setTimeout(this.test, 0);
 };
 
+var a = 10000;
+
 var f = new F();
+
+// f.test();
+setTimeout(f.test, 0);
